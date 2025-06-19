81f8bdc45ef6cc040988691c6dc4af8a0ba4363b
sungrazer <jonwmeyer@gmail.com>
2025-06-19 09:22:11 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@
     apt-get install -y build-essential
     apt-get install -y git
     wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz && rm go1.21.6.linux-amd64.tar.gz
-    ENV GOROOT=/usr/local/go
-    ENV GOPATH=/go
-    ENV PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-    ENV GO111MODULE=on
-    ENV CGO_ENABLED=1
+    export GOROOT=/usr/local/go
+    export GOPATH=/go
+    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+    export GO111MODULE=on
+    export CGO_ENABLED=1
     mkdir -p /go/src
     mkdir -p /go/bin
     cd /tmp && go install github.com/projectdiscovery/katana/cmd/katana@latest
