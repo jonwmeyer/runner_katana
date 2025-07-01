04f225c4583f6fa6db1a0c280d28f6efc1fa91fa
sungrazer <jonwmeyer@gmail.com>
2025-07-01 17:00:09 -0600

    .

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
     echo "START: Building Katana"
-    apt-get update
-    apt-get install -y ca-certificates
-    apt-get install -y build-essential
-    apt-get install -y git
+    apt update
+    apt install -y build-essential ca-certificates wget unzip git
+    apt install -y python3 python3-pip python-is-python3
     wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz && rm go1.21.6.linux-amd64.tar.gz
-    export GOROOT=/usr/local/go
-    export GOPATH=/go
-    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-    export GO111MODULE=on
-    export CGO_ENABLED=1
+    ENV GOROOT=/usr/local/go
+    ENV GOPATH=/go
+    ENV PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+    ENV GO111MODULE=on
+    ENV CGO_ENABLED=1
     mkdir -p /go/src
     mkdir -p /go/bin
     cd /tmp && go install github.com/projectdiscovery/katana/cmd/katana@latest
